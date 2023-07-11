1eebffa1e1e98b28299ca5ead12d7ad9626e71e5
896ma <solarcrash3@gmail.com>
2023-07-11 14:52:13 +0300

    python.c.C++

---
+++ loops.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    int count;
+    for (count=1;count<=12;count++)
+    {
+        printf("Go hard or go home!\n");
+    }
+    return 0;
+}
---
+++ shutdown/shutdown.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main (void)
+{
+    system("c:\\windows\\system 32\\shutdown /r");
+    return 0;
+}
